99e7ca12f06d760e02618d061e57292193ffd961
Mynor Lopez <mynlop15@gmail.com>
2020-10-05 19:17:26 -0600

    style components

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import H2Styled from '../styled/H2Styled';
+import H3Styled from '../styled/H3Styled';
+import PStyled from '../styled/PStyled';
+import PStyle from '../styled/PStyled';
+
 
 const Experience = props => (
   <div className="Experience">
+    <H2Styled name="Experience" />
     <div className="Experience-container">
 
       {props.data.map((exp, index) => (
         <div className="Experience-item" key={`Exp-${index}`}>
-	  <h3>{exp.jobTitle} {exp.company}
-	    <span>{exp.startDate}-{exp.endDate}</span>
-	    <span>{exp.jobDescription}</span>
-	  </h3>
-      </div>
+          <H3Styled>{exp.jobTitle} {exp.company} @
+             <span> {exp.startDate}-{exp.endDate}</span>
+          </H3Styled>
+          <PStyled name={exp.jobDescription}/>
+        </div>
       ))}
     </div>
   </div>
