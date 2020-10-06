54943a3c9ee475652cbfa5a27dc72883a9145118
Mynor Lopez <mynlop15@gmail.com>
2020-10-06 17:33:07 -0600

    seo e icono

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -31,7 +32,8 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./public/index.html",
       file: "./index.html"
-    })
+    }),
+    new FaviconsWebpackPlugin('./public/usuario.png')
   ]
 }
 
