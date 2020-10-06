7943c80c73eca46dba298398300eb007eda84621
Mynor Lopez <mynlop15@gmail.com>
2020-10-06 12:36:47 -0600

    animacion barra progreso

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled,{ keyframes} from 'styled-components'; 
 
 const SkillsContainer = styled.div`
   display: grid;
@@ -19,6 +19,11 @@ const SkillsLine = styled.div`
   background: #F8BBD0;
 `;
 
+const move = keyframes `
+  0% {background-position: 0 0; }
+  100% {background-position: 50px 50px}
+`;
+
 const SkillsSpan = styled.span`
   display: block;
   height: 100%;
@@ -27,7 +32,21 @@ const SkillsSpan = styled.span`
   background-color: #C2185B;
   overflow: hidden;
   position: relative;
-  box-shadow: inset 0 2px 9px rgba(255,255,255,0.3), inset 0 -2px 6rgba(0,0,0,0.4);
+  box-shadow: inset 0 2px 9px rgba(255,255,255,0.3), inset 0 -2px 6px rgba(0,0,0,0.4);
+  
+  &:after{
+    content: "";
+    position: absolute;
+    top:0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    background-image: linear-gradient(-45deg, rgba(255,255,255,0.2) 25%, transparent 25%, transparent 50%, rgba(255,255,255,0.2) 50%, rgba(255,255,255, 0.2) 75%, transparent 75%, transparent);
+    z-index: 1;
+    background-size: 50px 50px;
+    animation: ${move} 4s linear infinite;
+    overflow: hidden;
+  }
 `;
 
 const Skills = props => (
